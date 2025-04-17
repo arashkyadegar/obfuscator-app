721f92147631f2cd260dec4634ea46ed28988dc5
arashk <yadegar.arashk@gmail.com>
2025-04-17 12:35:01 -0700

    feat: sample file added

---
+++ source-files/sample.js
@@ -0,0 +1,19 @@
+// Sample JavaScript File
+
+// Function to add two numbers
+function add(a, b) {
+    return a + b;
+}
+
+// Function to greet a user
+function greet(name) {
+    return `Hello, ${name}! Welcome to the world of obfuscation!`;
+}
+
+// Main logic
+const num1 = 5;
+const num2 = 10;
+const userName = "Arashk";
+
+console.log(`The sum of ${num1} and ${num2} is:`, add(num1, num2));
+console.log(greet(userName));
